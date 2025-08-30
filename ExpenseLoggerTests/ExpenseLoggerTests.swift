1e695fbb9faf8ef461d9a81d1ff62bf92e15c9f7
mktowett <marvintowett@gmail.com>
2025-08-30 21:30:09 +0300

    Initial Commit

---
+++ ExpenseLoggerTests/ExpenseLoggerTests.swift
@@ -0,0 +1,16 @@
+//
+//  ExpenseLoggerTests.swift
+//  ExpenseLoggerTests
+//
+//  Created by marvin towett on 30/08/2025.
+//
+
+import Testing
+
+struct ExpenseLoggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
